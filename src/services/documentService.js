92eb6df35b53ec7a1aad49d1c964621aaf5dff13
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-04-16 23:49:57 +0530

    first commit with Updates in documentService.js and .env

--- src/services/documentService.js
+++ src/services/documentService.js
@@ -4,8 +4,28 @@ const API_URL = process.env.REACT_APP_API_URL;
 const user = JSON.parse(localStorage.getItem('user'));
 // Extract the token from the parsed object
 const token = user ? user.token : null;
+// Not working/ Document Not Get
+// export const getDocuments = async () => {
+//     const { data } = await axios.get(API_URL, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return data;
+// };
+
+// export const getDocumentById = async (id) => {
+//     const { data } = await axios.get(`${API_URL}/${id}`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return data;
+// };
+// Instead Used 
+
 export const getDocuments = async () => {
-    const { data } = await axios.get(API_URL, {
+    const { data } = await axios.get(`${API_URL}/api/documents`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
@@ -14,13 +34,14 @@ export const getDocuments = async () => {
 };
 
 export const getDocumentById = async (id) => {
-    const { data } = await axios.get(`${API_URL}/${id}`, {
+    const { data } = await axios.get(`${API_URL}/api/documents/${id}`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     });
     return data;
 };
+
 
 // Update a document by ID
 export const updateDocument = async (id, documentData) => {
