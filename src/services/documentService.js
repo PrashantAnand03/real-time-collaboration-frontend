c24ff7a6974abbb60cd4bfb690de1757128276de
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-04-18 09:59:32 +0530

    All files upload to test repo

--- src/services/documentService.js
+++ src/services/documentService.js
@@ -4,28 +4,10 @@ const API_URL = process.env.REACT_APP_API_URL;
 const user = JSON.parse(localStorage.getItem('user'));
 // Extract the token from the parsed object
 const token = user ? user.token : null;
-// Not working/ Document Not Get
-// export const getDocuments = async () => {
-//     const { data } = await axios.get(API_URL, {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//         },
-//     });
-//     return data;
-// };
 
-// export const getDocumentById = async (id) => {
-//     const { data } = await axios.get(`${API_URL}/${id}`, {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//         },
-//     });
-//     return data;
-// };
-// Instead Used 
-
+// Get a document by ID
 export const getDocuments = async () => {
-    const { data } = await axios.get(`${API_URL}/api/documents`, {
+    const { data } = await axios.get(API_URL, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
@@ -34,13 +16,34 @@ export const getDocuments = async () => {
 };
 
 export const getDocumentById = async (id) => {
-    const { data } = await axios.get(`${API_URL}/api/documents/${id}`, {
+    const { data } = await axios.get(`${API_URL}/${id}`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     });
     return data;
 };
+
+
+// Chat GPT suggested not correct still fixing
+
+// export const getDocuments = async () => {
+//     const { data } = await axios.get(`${API_URL}/api/documents`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return data;
+// };
+
+// export const getDocumentById = async (id) => {
+//     const { data } = await axios.get(`${API_URL}/api/documents/${id}`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return data;
+// };
 
 
 // Update a document by ID
