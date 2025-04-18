d23908912b1e83fa77dcbc98fcf5121a317bea44
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-04-18 11:04:48 +0530

    updated in index.js remove document.write

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap
 import App from './App';
 
 // Create the necessary HTML structure directly in JavaScript
-document.write(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Real-Time Collaboration Tool</title>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-  </html>
-`);
+// document.write(`
+//   <!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Real-Time Collaboration Tool</title>
+//   </head>
+//   <body>
+//     <div id="root"></div>
+//   </body>
+//   </html>
+// `);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
